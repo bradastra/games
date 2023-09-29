4f38df5a5b1681552404cf56925caa1f4db5ce72
bradastra <bradastra.ai@gmail.com>
2023-09-29 11:26:54 -0600

    Update for a more robust gameplay agains the computer

--- farkle.py
+++ farkle.py
@@ -19,88 +19,89 @@ def roll_dice(num=6):
 
 def calculate_score(dice):
     score = 0
-    dice_counts = {i: dice.count(i) for i in set(dice)}
-
     for combo, points in SCORING_RULES.items():
-        if all(dice_counts.get(d, 0) >= combo.count(d) for d in combo):
+        if all(dice.count(d) >= combo.count(d) for d in combo):
             score += points
+            for d in combo:
+                dice.remove(d)
 
     return score
 
 def player_turn():
-    total_score = 0
-    dice_left = 6
+    remaining_dice = 6
+    turn_total = 0
 
-    while dice_left > 0:
-        current_roll = roll_dice(dice_left)
+    while remaining_dice > 0:
+        current_roll = roll_dice(remaining_dice)
         print(f"Current roll: {current_roll}")
 
-        while True:
-            selected_values = input("Which dice do you want to keep? (enter space-separated numbers or 'done'): ").split()
-            if selected_values == ['done']:
-                return total_score
+        chosen_dice = input("Which dice do you want to keep? (enter space-separated numbers or 'done'): ").split()
 
-            if not all(value.isdigit() and 1 <= int(value) <= dice_left for value in selected_values):
-                print("Please enter valid dice numbers or 'done'.")
-                continue
+        if "done" in chosen_dice:
+            break
 
-            try:
-                selected_values = tuple(current_roll[int(val) - 1] for val in selected_values)
-                if len(set(selected_values)) != len(selected_values):
-                    raise IndexError
-            except (ValueError, IndexError):
-                print("Invalid choice. Ensure you select dice numbers only from the current roll and don't duplicate. Try again.")
-                continue
-
-            score = calculate_score(selected_values)
+        valid_choice = all(current_roll.count(int(d)) >= chosen_dice.count(d) for d in chosen_dice)
+        if valid_choice:
+            score = calculate_score(list(map(int, chosen_dice)))
             if score == 0:
-                print("The selected dice don't give any score. Try again.")
-                continue
+                return 0
+            turn_total += score
+            remaining_dice -= len(chosen_dice)
+        else:
+            print("Invalid choice. Ensure you select dice numbers only from the current roll and don't duplicate. Try again.")
 
-            total_score += score
-            dice_left -= len(selected_values)
-
-    return total_score
+    return turn_total
 
 def computer_turn():
-    # ... [No changes here, keeping the existing code for computer_turn]
-    pass
+    # A very basic strategy for the computer: roll, take all points, and stop.
+    dice = roll_dice()
+    return calculate_score(list(dice))
 
 def display_rules():
-    # ... [No changes here, keeping the existing code for display_rules]
-    pass
+    print("--- FARKLE RULES & INSTRUCTIONS ---")
+    print("Objective: Be the first to score 10,000 points.")
+    print("Gameplay:")
+    print("1. Roll all six dice.")
+    print("2. After each roll, choose the dice you want to keep (the ones that give you scores).")
+    print("3. You can continue rolling the remaining dice to accumulate more points, but if a roll results in zero points, you lose all points accumulated in that turn.")
+    print("4. You decide when to stop rolling and keep the points you've accumulated.")
+    print("\nScoring:")
+    for combo, points in SCORING_RULES.items():
+        print(f"{combo}: {points} points")
 
-def play_again_prompt():
-    """Prompts the player to either play again or exit the game."""
-    time.sleep(2)  # Wait for 2 seconds before asking
-    while True:
-        choice = input("Play Again? Y / N (or X to exit): ").lower()
-        if choice == 'y':
-            return True
-        elif choice == 'n' or choice == 'x':
-            return False
-        else:
-            print("Invalid choice. Please choose Y, N, or X.")
+def play_game():
+    player_score = 0
+    computer_score = 0
+
+    while player_score < 10000 and computer_score < 10000:
+        print("\nYour turn!")
+        player_score += player_turn()
+        print(f"Your turn ended with {player_score} points.\n")
+
+        print("Computer's turn...")
+        computer_score += computer_turn()
+        print(f"Computer's turn ended with {computer_score} points.\n")
+
+    if player_score >= 10000:
+        print("You Win!")
+    else:
+        print("Computer Wins!")
+    time.sleep(2)
 
 def main():
+    print("Welcome to Farkle!")
+    time.sleep(2)
     display_rules()
-    
-    while True:
-        player_score, computer_score = 0, 0
+    play_choice = input("\nWould you like to play? (Y to start / X to exit): ").lower()
 
-        while player_score < 10000 and computer_score < 10000:
-            player_score += player_turn()
-            print(f"Your turn ended with {player_score} points.")
-            
-            computer_score += computer_turn()
-            print(f"Computer's turn ended with {computer_score} points.")
+    while play_choice == 'y':
+        play_game()
+        play_choice = input("\nPlay Again? Y / N (or X to exit): ").lower()
 
-        winner = "Player" if player_score >= 10000 else "Computer"
-        print(f"{winner} wins!")
-
-        if not play_again_prompt():
-            print("Thanks for playing!")
-            break
+    print("\nThank you for playing Farkle! It was a pleasure to have you.")
+    print("- bradastra")
+    print("\nP.S. Check out my Instagram profile: @bradastra.ai. Hope to see you there!")
 
 if __name__ == "__main__":
     main()
+
