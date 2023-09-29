f60b4fbabc9f85ffc0919fe6825b489ba59f2b74
bradastra <146237232+bradastra@users.noreply.github.com>
2023-09-29 12:11:58 -0600

    Updated Farkle game with improved computer strategy

--- farkle.py
+++ farkle.py
@@ -1,10 +1,55 @@
 import random
 import time
 
-# ... [unchanged code]
+# Scoring rules
+SCORING_RULES = {
+    (1,): 100,
+    (5,): 50,
+    (1, 1, 1): 1000,
+    (2, 2, 2): 200,
+    (3, 3, 3): 300,
+    (4, 4, 4): 400,
+    (5, 5, 5): 500,
+    (6, 6, 6): 600,
+    (1, 2, 3, 4, 5, 6): 1500
+}
+
+def roll_dice(num=6):
+    return tuple(random.randint(1, 6) for _ in range(num))
+
+def calculate_score(dice):
+    score = 0
+    for combo, points in SCORING_RULES.items():
+        if all(dice.count(d) >= combo.count(d) for d in combo):
+            score += points
+            for d in combo:
+                dice.remove(d)
+    return score
 
 def player_turn():
-    # ... [unchanged code]
+    remaining_dice = 6
+    turn_total = 0
+
+    while remaining_dice > 0:
+        current_roll = roll_dice(remaining_dice)
+        print(f"Current roll: {current_roll}")
+
+        chosen_dice = input("Which dice do you want to keep? (enter space-separated numbers or 'done'): ").split()
+
+        if "done" in chosen_dice:
+            break
+
+        valid_choice = all(current_roll.count(int(d)) >= chosen_dice.count(d) for d in chosen_dice)
+        if valid_choice:
+            score = calculate_score(list(map(int, chosen_dice)))
+            if score == 0:
+                return 0
+            turn_total += score
+            remaining_dice -= len(chosen_dice)
+        else:
+            print("Invalid choice. Ensure you select dice numbers only from the current roll and don't duplicate. Try again.")
+
+    return turn_total
 
 # New Strategy and Turn Functions
 def computer_strategy(player_score, computer_score):
@@ -55,10 +100,21 @@ def play_game():
         computer_score += computer_turn(player_score, computer_score)
         print(f"Computer's turn ended with {computer_score} points.\n")
 
-    # ... [rest of the code remains unchanged]
+    if player_score >= 10000:
+        print("Congratulations! You win!")
+    else:
+        print("Computer wins! Better luck next time.")
 
-# ... [rest of the code, like display_rules, main, etc.]
+def display_rules():
+    print("Welcome to Farkle!")
+    print("\nThe scoring rules are:")
+    for combo, score in SCORING_RULES.items():
+        print(f"{combo}: {score} points")
+    print("\nThe first player to reach 10,000 points wins!")
+
+def main():
+    display_rules()
+    play_game()
 
 if __name__ == "__main__":
     main()
-
