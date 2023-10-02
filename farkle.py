7a071f86aac4b7abf536e05d44dd962aff01b38b
bradastra <bradastra.ai@gmail.com>
2023-10-02 14:32:26 -0600

    Updated farkle.py with enhanced scoring mechanism and user experience improvements.

--- farkle.py
+++ farkle.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-# Version: 2.0
+# Version: 2.1
 
 # Scoring rules
 SCORING_RULES = {
@@ -13,14 +13,34 @@ SCORING_RULES = {
     (4, 4, 4): 400,
     (5, 5, 5): 500,
     (6, 6, 6): 600,
-    (1, 1, 1, 1): 2000,
-    (2, 2, 2, 2): 400,
-    (3, 3, 3, 3): 600,
-    (4, 4, 4, 4): 800,
-    (5, 5, 5, 5): 1000,
-    (6, 6, 6, 6): 1200,
-    (1, 1, 1, 1, 1): 3000,
-    (1, 1, 1, 1, 1, 1): 4000,
+    (1, 2, 3, 4, 5, 6): 1500,  # Straight
+    (1, 1, 2, 2, 3, 3): 1500,  # Three pairs variations
+    (1, 1, 4, 4, 5, 5): 1500,
+    (1, 1, 3, 3, 6, 6): 1500,
+    (2, 2, 4, 4, 6, 6): 1500,
+    (2, 2, 3, 3, 5, 5): 1500,
+    (1, 1, 1, 2, 2, 2): 2500,  # Two triplets variations
+    (1, 1, 1, 3, 3, 3): 2500,
+    (1, 1, 1, 4, 4, 4): 2500,
+    (1, 1, 1, 5, 5, 5): 2500,
+    (1, 1, 1, 6, 6, 6): 2500,
+    (2, 2, 2, 3, 3, 3): 2500,
+    (2, 2, 2, 4, 4, 4): 2500,
+    (2, 2, 2, 5, 5, 5): 2500,
+    (2, 2, 2, 6, 6, 6): 2500,
+    (3, 3, 3, 4, 4, 4): 2500,
+    (3, 3, 3, 5, 5, 5): 2500,
+    (3, 3, 3, 6, 6, 6): 2500,
+    (4, 4, 4, 5, 5, 5): 2500,
+    (4, 4, 4, 6, 6, 6): 2500,
+    (5, 5, 5, 6, 6, 6): 2500,
+}
+
+# Four, Five, and Six of a kind multiplier rules
+MULTIPLIER_RULES = {
+    4: 2,
+    5: 3,
+    6: 4,
 }
 
 def roll_dice(num=6):
@@ -28,119 +48,85 @@ def roll_dice(num=6):
 
 def calculate_score(dice):
     score = 0
+    dice_count = {i: dice.count(i) for i in range(1, 7)}
+
     for combo, combo_score in SCORING_RULES.items():
         if all(dice.count(d) >= combo.count(d) for d in combo):
             score += combo_score
+
+    # Checking for 4, 5, and 6 of a kind
+    for num, count in dice_count.items():
+        if count > 3:
+            base_score = SCORING_RULES.get((num, num, num), 0)
+            score += base_score * MULTIPLIER_RULES[count]
+
     return score
 
+def is_farkle(dice):
+    return calculate_score(dice) == 0
+
 def player_turn():
-    remaining_dice = 6
-    turn_score = 0
-    while remaining_dice > 0:
-        roll = roll_dice(remaining_dice)
-        print(f"\nCurrent roll: {roll}")
-        
-        choices = input("Which dice do you want to keep? (enter space-separated numbers, 'done' to end turn, or 'exit' to quit game): ").lower().split()
-
-        if "exit" in choices:
-            print("Thank you for playing! Goodbye!")
-            exit()
-
-        if "done" in choices:
-            return turn_score
-
-        # Calculate score based on player's choices
-        chosen_dice = tuple(map(int, choices))
-        score_for_this_roll = calculate_score(chosen_dice)
-        if score_for_this_roll == 0:
-            print("\nFarkle!\n")
+    total_turn_score = 0
+    num_dice = 6
+    input("\nYour turn! Press Enter to roll the dice...")
+    while True:
+        rolled_dice = roll_dice(num_dice)
+        print(f"\nYou rolled: {rolled_dice}")
+        score_this_roll = calculate_score(rolled_dice)
+        if is_farkle(rolled_dice):
+            print("Farkle! No points this turn.")
             return 0
-
-        turn_score += score_for_this_roll
-
-        for d in chosen_dice:
-            roll = list(roll)
-            roll.remove(d)
-            remaining_dice -= 1
-
-        if remaining_dice == 0:
-            remaining_dice = 6
-
-        print(f"You kept: {chosen_dice} - Current turn score: {turn_score}")
+        print(f"Score this roll: {score_this_roll}")
+        action = input("\nDo you want to (P)ass or (R)oll again? ").upper()
+        if action == 'P':
+            return total_turn_score + score_this_roll
+        elif action == 'R':
+            total_turn_score += score_this_roll
+            num_dice -= len(rolled_dice)
+            if num_dice == 0:
+                num_dice = 6
 
 def computer_turn():
-    remaining_dice = 6
-    turn_score = 0
-    while remaining_dice > 0:
-        roll = roll_dice(remaining_dice)
-        print(f"\nComputer rolled: {roll}")
-        time.sleep(2)
-
-        scoring_dice = [d for d in roll if calculate_score((d,)) > 0]
-        if not scoring_dice:
-            print("\nFarkle!\n")
+    total_turn_score = 0
+    num_dice = 6
+    print("\nComputer's turn!")
+    while total_turn_score < 300:  # Computer strategy to stop at a threshold score
+        time.sleep(2)  # Pause for suspense
+        rolled_dice = roll_dice(num_dice)
+        print(f"\nComputer rolled: {rolled_dice}")
+        score_this_roll = calculate_score(rolled_dice)
+        if is_farkle(rolled_dice):
+            print("Farkle! No points this turn for the computer.")
             return 0
-
-        turn_score += calculate_score(roll)
-        
-        for d in scoring_dice:
-            roll = list(roll)
-            roll.remove(d)
-            remaining_dice -= 1
-
-        if remaining_dice == 0:
-            remaining_dice = 6
-
-        print(f"Computer kept: {scoring_dice} - Current turn score: {turn_score}")
-        time.sleep(1)
-
-    return turn_score
-
-def display_rules():
-    print("""
-Welcome to Farkle!
-
-Scoring Rules:
-1 - 100 points
-5 - 50 points
-Three of a Kind (e.g., 1,1,1) - Based on the number (Three 1's: 1000, Three 2's: 200, etc.)
-Four of a Kind (e.g., 1,1,1,1) - Double the Three of a Kind score
-Five of a Kind - Triple the Three of a Kind score
-Six of a Kind - Quadruple the Three of a Kind score
-
-You can choose to keep rolling the dice for more points, but if you roll and don't score, you Farkle and lose your turn's points!
-
-At any point, enter 'done' to end your turn or 'exit' to quit the game.
-
-The first player to reach 10,000 points wins!
-
-Let's get started!
-""")
+        total_turn_score += score_this_roll
+        num_dice -= len(rolled_dice)
+        if num_dice == 0:
+            num_dice = 6
+    print(f"\nComputer secures {total_turn_score} points this turn.")
+    return total_turn_score
 
 def main():
-    display_rules()
-    
-    action = input("Do you want to 'play' or 'exit'? ").lower()
-    if action == 'exit':
-        print("Thank you for checking out Farkle! Goodbye!")
-        return
-
-    player_score = 0
-    computer_score = 0
-
-    while player_score < 10000 and computer_score < 10000:
-        print(f"\nCurrent Scores - You: {player_score}, Computer: {computer_score}\n")
-        
-        player_score += player_turn()
-        print(f"\nEnd of Your Turn - Your score: {player_score}\n")
-        
-        computer_score += computer_turn()
-        print(f"\nEnd of Computer's Turn - Computer score: {computer_score}\n")
-
-    if player_score >= 10000:
-        print("\nCongratulations! You won!")
-    else:
-        print("\nComputer wins. Better luck next time!")
+    player_score, computer_score = 0, 0
+    print("\nWelcome to Farkle!")
+    while True:
+        action = input("\nDo you want to (P)lay or (E)xit? ").upper()
+        if action == 'E':
+            print("\nThanks for playing!")
+            break
+        elif action == 'P':
+            while player_score < 10000 and computer_score < 10000:
+                player_score += player_turn()
+                print(f"\nYour total score: {player_score}")
+                computer_score += computer_turn()
+                print(f"Computer's total score: {computer_score}")
+            if player_score > computer_score:
+                print("\nCongratulations! You win!")
+            else:
+                print("\nComputer wins! Better luck next time!")
+            player_score, computer_score = 0, 0  # Reset scores for a new game
+        else:
+            print("\nInvalid choice. Please choose (P)lay or (E)xit.")
 
 if __name__ == "__main__":
     main()
+
